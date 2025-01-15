db28e0051fa8ce17053e4f27a023afadc860815f
Modyev <modyradwanedu@gmail.com>
2025-01-15 19:33:54 +0200

    Removed a Junk .cpp File

--- Angle Quadrant Calculator/Angle Quadrant Calculator.cpp
+++ Angle Quadrant Calculator/Angle Quadrant Calculator.cpp
@@ -1,65 +1,53 @@
-#include <iostream>
+// Angle Quadrant Calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
 
+#include <iostream>
 using namespace std;
 
+
 int Quadrant(int angle);
-
-int main() {
-
-	// 500
-	cout << "Enter an angle: ";
+int main()
+{
+	//330
 	int angle;
+	cout << "Enter your angle: ";
 	cin >> angle;
 
-
-	if (angle < 0) {
-		//angle = 360 + angle;
-	}
-	else if (angle > 0) {
-		int TempAngle = angle;
-
-		while (TempAngle > 360) {
-			TempAngle -= 360;
+	if (angle > 0) {
+		int tempAngle = angle;
+		while (tempAngle > 360) {
+			tempAngle -= 360;
 		}
-		int quad = Quadrant(TempAngle);
-
-		switch (quad) {
-			cout << endl;
+		switch (Quadrant(tempAngle)) {
 			case 1:
-				cout << "[1st Quadrant] The angle \'" << angle << "\' is in the 1st Quadrant (\"" << TempAngle << "\")\n";
+				cout << "Your Angle " << angle << " lies in the 1st Quadrant" << endl;
 				break;
 			case 2:
-				cout << "[2nd Quadrant] The angle \'" << angle << "\' is in the 2nd Quadrant (\"" << TempAngle << "\")\n";
-				break;
+				cout << "Your Angle " << angle << "lies in the 2nd Quadrant" << endl;
+	
 			case 3:
-				cout << "[3rd Quadrant] The angle \'" << angle << "\' is in the 3rd Quadrant (\"" << TempAngle << "\")\n";
+				cout << "Your Angle " << angle << " lies in the 3rd Quadrant" << endl;
 				break;
 			case 4:
-				cout << "[4th Quadrant] The angle \'" << angle << "\' is in the 4th Quadrant (\"" << TempAngle << "\")\n";
-				break;
+				cout << "Your Angle " << angle << " lies in the 4th Quadrant" << endl;
 			case -1:
-				cout << "[Special Case] The angle \'" << angle << "\' is a special case and it's not on a specific quadrant";
-				break;
+				cout << "Your Angle " << angle << " Doesn't lie on any quadrant because it's an edge case\n";
 		}
 	}
-
-
-
-	return 0;
 
 }
 
 int Quadrant(int angle) {
-	if (angle >= 0 && angle < 90) {
+	if (angle > 0 && angle < 90) {
 		return 1;
 	}
-	else if (angle >= 90 && angle < 180) {
+	else if (angle > 90 && angle < 180) {
 		return 2;
 	}
-	else if (angle >= 180 && angle < 270) {
+	else if (angle > 180 && angle < 270) {
 		return 3;
 	}
-	else if (angle >= 270 && angle < 360) {
+	else if (angle > 270 && angle < 360) {
 		return 4;
 	}
 	else {
